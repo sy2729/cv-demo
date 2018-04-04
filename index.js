ba101f32c60660ff665f959177dc2b6686be2e50
sy2729 <sy2729@tc.columbia.edu>
2018-04-03 23:43:17 -0400

    add vallina js in-page scrolling aniamtion initially

--- index.js
+++ index.js
@@ -33,17 +33,36 @@ for(let i = 0; i < navs.length; i++) {
 
 //in-page auto scrolling with js
 var siteHrefs = document.querySelectorAll('.nav-sub li a');
+
+
+
+
 for(let i = 0; i < siteHrefs.length; i++) {
     siteHrefs[i].onclick = function(e) {
         var href = e.currentTarget.getAttribute('href');
         var targetedEl = document.querySelector(href);
-        var elDistance = targetedEl.offsetTop
-        window.scrollTo(0, elDistance - 100);
+        // var elDistance = targetedEl.offsetTop
+
+        let n = 25;        //move times in total
+        let duration = 500/n         //the duration between each move
+        let currentTop = window.scrollY;
+        let elementTop = targetedEl.offsetTop - 100;
+        let distance = (elementTop - currentTop) / n;
+        let i = 0;
+        var id = setInterval(function () {
+            console.log(distance)
+            if (i < n) {
+                i++;
+                window.scrollTo(0, currentTop + distance * i)
+            } else {
+                window.clearInterval(id)
+                return
+            }
+        }, duration)
+        // window.scrollTo(0, elDistance - 100);
         e.preventDefault();
     }
 }
-
-
 
 
 
