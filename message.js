aca07f43629e1c3c51ca6c577897f03402caf4b0
sy2729 <sy2729@tc.columbia.edu>
2018-06-03 23:35:14 -0400

    add MVC framework

--- message.js
+++ message.js
@@ -1,69 +1,89 @@
 
 setTimeout(() => {
-    init();
+    controller.init(view);
 }, 0);
 
-
-
-function init() {
-    var APP_ID = 'iUAUdmSeizUpHyu2GxiO0txO-gzGzoHsz';
-    var APP_KEY = '92mhpDbB13kiEKSswYht11OW';
-    AV.init({
-        appId: APP_ID,
-        appKey: APP_KEY,
-    });
-
-    var query = new AV.Query('Message');
-    query.find().then(function (results) {
-       var message = results.map((i)=>{
-        return i.attributes
-       })
-    let messageList = view.querySelector('.message-list')
-    message.forEach((i)=>{
-        let li = document.createElement('li');
-        li.textContent = i.words;
-        messageList.appendChild(li);
-    })
-
-
-    }, function (error) {
-        // error is an instance of AVError.
-    });
-}
+var view = document.querySelector('.message-board');
 
 
 
-
-
-let view = document.querySelector('.message-board');
-
-
-let button = view.querySelector('.leaveMessage');
-button.onsubmit = function (e) {
-    e.preventDefault();
-    var Message = AV.Object.extend('Message');
-    var message = new Message();
-    var value = view.querySelector('.message-content').value;
-    message.save({
-        words: value
-    }).then(function (object) {
-        alert('submit successfully')
-        let li = document.createElement('li');
-        let messageList = view.querySelector('.message-list')
-        li.textContent = view.querySelector('.message-content').value;
-        messageList.appendChild(li);
-        view.querySelector("input[type='text']").value = '';
-    })
-}
-
-let controller = {
-    
-};
 
 
 let model = {
-    list: null,
-    getData: ()=>{
-        this.list = view.querySelector('.message-list');
+    fetch: function(){
+        var query = new AV.Query('Message');
+        return query.find();
+    },
+
+    save: function(name, value) {
+        var Message = AV.Object.extend('Message');
+        var message = new Message();
+        return message.save({
+            name: name,
+            words: value
+        })
     }
+};
+
+
+
+
+
+let controller = {
+    view: null,
+    init: function(view) {
+        this.view = view;
+        this.form = view.querySelector('form');
+        this.messageList = view.querySelector('.message-list');
+        this.initAv();
+        this.loadMessage();
+        this.bindEvent();
+        // this
+    },
+    initAv: function(){
+        var APP_ID = 'iUAUdmSeizUpHyu2GxiO0txO-gzGzoHsz';
+        var APP_KEY = '92mhpDbB13kiEKSswYht11OW';
+        AV.init({
+            appId: APP_ID,
+            appKey: APP_KEY,
+        });
+    },
+    loadMessage: function(){
+       model.fetch().then( (results)=> {
+            var message = results.map((i) => {
+                return i.attributes
+            })
+            
+            message.forEach((i) => {
+                let li = document.createElement('li');
+                li.textContent = `${i.name||'N/A'} : ${i.words}`;
+                this.messageList.appendChild(li);
+            })
+        });      
+    },
+
+    bindEvent: function(){
+        this.form.onsubmit = (e)=> {
+          e.preventDefault();
+          this.saveMessage();  
+        }
+    },
+
+    saveMessage: function() {
+        let myForm = this.form;
+        let messageContent = myForm.querySelector('.message-content');
+        let nameEl = myForm.querySelector('.name-content');
+        let name = nameEl.value;
+        var value = messageContent.value;
+        model.save(name, value).then((object)=> {
+            alert('submit successfully')
+            let li = document.createElement('li');
+            li.textContent = nameEl.value + ": " + messageContent.value;
+            this.messageList.appendChild(li);
+            nameEl.value = '';
+            messageContent.value = '';
+        })
+    }
+
+
 };
